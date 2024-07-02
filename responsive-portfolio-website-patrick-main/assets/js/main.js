f03366ba91f0a1083cdda307bedad0591b8043e2
BansariSanghvi <Bansari.Sanghvi@city.ac.uk>
2024-07-02 17:43:56 +0100

    Added the About Me part

--- responsive-portfolio-website-patrick-main/assets/js/main.js
+++ responsive-portfolio-website-patrick-main/assets/js/main.js
@@ -38,6 +38,13 @@ const scrollHeader = () => {
 
 /*=============== ADD BLUR TO HEADER ===============*/
 
+const blurHeader = () => {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add blur hear to header tag
+    this.scrollY >=50 ? header.classList.add('blur-header') : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
+
 
 /*=============== EMAIL JS ===============*/
 
