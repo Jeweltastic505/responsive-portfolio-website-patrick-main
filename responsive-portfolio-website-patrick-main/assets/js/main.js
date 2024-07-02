5e8333b9799c7e5dcb9815cfaec0a9a6715d0c3c
BansariSanghvi <Bansari.Sanghvi@city.ac.uk>
2024-07-02 15:31:25 +0100

    Formed the home page section

--- responsive-portfolio-website-patrick-main/assets/js/main.js
+++ responsive-portfolio-website-patrick-main/assets/js/main.js
@@ -1,8 +1,40 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle')          
+      navClose = document.getElementById('nav-close')
 
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu')
+    })
+    
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
+const navLink = document.querySelectorAll('.nav_link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*===============CHANGE BACKGROUND HEADER =============*/
+
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add scroll hear to header tag
+    this.scrollY >=50 ? header.classList.add('scroll-header') : header.classList.remove('scroll-header')
+}
 
 /*=============== ADD BLUR TO HEADER ===============*/
 
